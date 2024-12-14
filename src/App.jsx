d8d3e5ccba80a1fbf763d61a2c24b7e03e0e6981
Linn Nay Chi Hlaing <126392986+Veroni02@users.noreply.github.com>
2024-12-14 20:28:48 +0630

    created react weather app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <div className="container">
+          <Weather defaultCity="San Francisco" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
